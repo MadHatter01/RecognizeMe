19bfdde65bf8e08dd60c49511afce79ce4b49dc6
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-04-20 16:47:41 -0700

    Image Upload logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
-
+import React, {useState} from 'react';
+// import axios from 'axios';
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [imageUrl, setImageUrl] = useState('');
+
+  const handleFileChange = (event)=>{
+    setSelectedFile(event.target.files[0]);
+    setImageUrl(URL.createObjectURL(event.target.files[0]))
+    console.log(selectedFile);
+    console.log(imageUrl)
+  }
+  const uploadImage = ()=>{
+    console.log('functionality to be added');
+  }
+
   return (
     <div className="App">
-      <h1>Test Page</h1>
+      <h1>Recognize Me</h1>
+      <input type="file" accept ="image/*" onChange={handleFileChange} />
+      <button onClick={uploadImage}>Analyze Image</button>
+      {imageUrl && <img src={imageUrl} alt="uploaded file" />}
     </div>
   );
 }
